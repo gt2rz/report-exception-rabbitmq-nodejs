4d8bc8aee86cbaa1e6cb0a1df0ed47af3fd863c0
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-11-06 17:05:24 -0500

    Add RabbitMQService class with methods for connecting, consuming, and sending messages.

--- src/services/rabbitmq.ts
+++ src/services/rabbitmq.ts
@@ -1,11 +1,27 @@
 import client, { Connection, Channel, ConsumeMessage } from 'amqplib';
 
 export class RabbitMQService {
+  /**
+   * The AMQP connection to the RabbitMQ server.
+   */
   private connection: Connection | null = null;
+  
+  /**
+   * The AMQP channel used for communication with the RabbitMQ server.
+   */
   private channel: Channel | null = null;
 
+  /**
+   * Creates a new instance of the RabbitMQ service.
+   * @param url The URL of the RabbitMQ server.
+   */
   constructor(private url: string) {}
 
+  /**
+   * Connects to the RabbitMQ server.
+   * @returns {Promise<void>}
+   * @throws {Error} If the connection fails.
+   */
   async connect() {
     try {
       this.connection = await client.connect(this.url);
@@ -16,6 +32,13 @@ export class RabbitMQService {
     }
   }
 
+  /**
+   * Consume messages from a queue.
+   * @param queue - The name of the queue to consume messages from.
+   * @param onMessage - A callback function to handle each message received from the queue.
+   * The function receives a `ConsumeMessage` object or `null` if the consumer was cancelled or closed.
+   * @throws An error if the channel is not initialized or has been closed, or if the connection is not initialized.
+   */
   async consume(queue: string, onMessage: (msg: ConsumeMessage | null) => void) {
     if (!this.channel) {
       throw new Error('Channel is not initialized or has been closed.');
@@ -29,6 +52,12 @@ export class RabbitMQService {
     await this.channel.consume(queue, onMessage);
   }
 
+  /**
+   * Sends a message to the specified queue.
+   * @param queue - The name of the queue to send the message to.
+   * @param message - The message to send to the queue.
+   * @throws Error if the channel is not initialized or has been closed.
+   */
   async sendToQueue(queue: string, message: string) {
     if (!this.channel) {
       throw new Error('Channel is not initialized or has been closed.');
@@ -38,6 +67,9 @@ export class RabbitMQService {
     this.channel.sendToQueue(queue, Buffer.from(message));
   }
 
+  /**
+   * Closes the RabbitMQ connection if it exists.
+   */
   async close() {
     if (this.connection) {
       await this.connection.close();
