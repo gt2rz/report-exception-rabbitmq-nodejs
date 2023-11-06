0123a548850af8c940719cb405d5a9ad4fd5a123
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-11-06 14:43:32 -0500

    Add dotenv dependency and update RabbitMQ connection string

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import dotenv from 'dotenv';
 import { RabbitMQService } from './services/rabbitmq.js';
 
-const rabbitMQStringConnection = 'amqp://guest:secret@rabbitmq:5672';
+dotenv.config();
+
+const rabbitMQStringConnection = `amqp://${process.env.RABBITMQ_USERNAME}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`;
 const rabbitMQService = new RabbitMQService(rabbitMQStringConnection);
 
 await rabbitMQService.connect();
+
+console.log('Connected to RabbitMQ');
