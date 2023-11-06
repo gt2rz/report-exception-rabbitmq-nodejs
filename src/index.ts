4e09adae997a605d60520ba86150a7015ca947f1
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-11-06 12:20:22 -0500

    Add RabbitMQ service connection to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello, world!");
+import { RabbitMQService } from '@/services/rabbitmq.js';
+
+const rabbitMQStringConnection = 'amqp://localhost:5672';
+const rabbitMQService = new RabbitMQService(rabbitMQStringConnection);
+
+await rabbitMQService.connect();
